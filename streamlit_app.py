fe040f554162ae18dcb53cd3cf9127bd140ac37e
prtkbhtt <85171432+prtkbhtt@users.noreply.github.com>
2022-08-30 15:31:10 +0530

    Update streamlit_app.py
    added pandas library
    added line chart line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
+import pandas pd
 
 streamlit.title('my parants')
+
+df = pd.read_csv("C:\Users\prateek.bhatt\Downloads\veg_plant_height.csv")
+streamlit.line_chart(df)                   
+
