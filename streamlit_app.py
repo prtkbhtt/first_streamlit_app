ef9346bff38786bef9e835cc50e3c6901d7a960b
prtkbhtt <85171432+prtkbhtt@users.noreply.github.com>
2022-08-30 15:39:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import pandas as pd
 streamlit.title("""My Web Application
       *Hellow World!*""")
 
-df = pd.read_csv("C:\Users\prateek.bhatt\Downloads\veg_plant_height.csv")
+df = pd.read_csv("C:\Users\prateek.bhatt\Downloads\veg_plant_height")
 streamlit.line_chart(df)                   
 
