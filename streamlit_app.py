4cdd8ac8c6df705e2f6e45c9f3fe0a662967b671
prtkbhtt <85171432+prtkbhtt@users.noreply.github.com>
2022-08-30 16:07:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
-import streamlit
+import streamlit as st
 import pandas as pd
 
 streamlit.title("""
 # My Web Application
       Hello World!*
       """)
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
 
 
