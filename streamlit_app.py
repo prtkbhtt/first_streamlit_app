3ef79eac455d58112ff9b1d6f1be80ca24c03dd7
prtkbhtt <85171432+prtkbhtt@users.noreply.github.com>
2022-08-30 16:14:44 +0530

    Update streamlit_app.py
    added pandas 
    add dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit 
+import pandas
 
 streamlit.title("My first webpage")
 
@@ -8,4 +9,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
