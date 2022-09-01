feee97fc1ce631f00ecc1806fb1039cb66aa29f7
prtkbhtt <85171432+prtkbhtt@users.noreply.github.com>
2022-09-01 18:35:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-streamlit.write('The user entered ', fruit_choice)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(fruityvice_response.index))
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+#streamlit.write('The user entered ', fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
