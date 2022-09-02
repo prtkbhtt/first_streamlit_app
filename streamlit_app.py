980e4a7cc7e763b7951c41f78b2a71ba384a7030
prtkbhtt <85171432+prtkbhtt@users.noreply.github.com>
2022-09-02 15:12:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit 
 import pandas
 import requests
-#import snowflake.connector
+
 
 streamlit.title("My first webpage")
 
@@ -36,4 +36,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
 
